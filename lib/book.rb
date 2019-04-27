9ed22ee780b0f7db03f832c0f58b50b631ec57eb
Jim Hough <teachlife@outlook.com>
2019-04-27 13:48:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,4 +5,16 @@ class Book
   def title
     title
   end
+  def author=(author)
+    @author = author
+  end
+  def author
+    @author
+  end
+  def page_count= (page_count)
+    @page_count = page_count
+  end
+  def page_count
+    @page_count
+  end
 end
